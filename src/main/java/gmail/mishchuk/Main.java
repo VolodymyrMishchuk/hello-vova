b538eefbba9a7bd413c3df139394f617c1f2b297
VolodymyrMishchuk <pidsercem@gmail.com>
2025-02-21 17:37:01 +0100

    2

--- src/main/java/gmail/mishchuk/Main.java
+++ src/main/java/gmail/mishchuk/Main.java
@@ -2,6 +2,6 @@ package gmail.mishchuk;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello, World!");
+        System.out.printf("Hello, World!2");
     }
 }
